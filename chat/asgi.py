f9ec771b7761626b160ac24367c7c838c8ba3e0b
unknown <hiahmadyan@gmail.com>
2022-06-19 14:32:44 +0430

    connection asgi application to websocker url patterns

--- chat/asgi.py
+++ chat/asgi.py
@@ -9,12 +9,22 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+
+from baseapp.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    # Just HTTP for now. (We can add other protocols later.)
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                websocket_urlpatterns
+            )
+        )
+    )
 })
