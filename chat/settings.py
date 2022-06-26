2ef001d65f45dc95edd470ff15ee31cbf666bae2
unknown <hiahmadyan@gmail.com>
2022-06-26 18:43:07 +0430

    adding tests to baseapp

--- chat/settings.py
+++ chat/settings.py
@@ -90,6 +90,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'TEST': {
+            'NAME': Path.joinpath(BASE_DIR, 'db_test.sqlite3')}
     }
 }
 
