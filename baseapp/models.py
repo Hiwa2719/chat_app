a97cfaea92cd5af30e5d66387dffd7ad67311b94
unknown <hiahmadyan@gmail.com>
2022-06-26 19:40:03 +0430

    adding Message model

---
+++ baseapp/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Message(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_messages')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.author.username
+
+    def last_10_messages(self):
+        return Message.objects.order_by('-timestamp')[:10]
