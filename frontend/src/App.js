7c3581cd6b4b6aa78baa9cf0d07469bc409e7a4b
hiwa2719 <hiahmadyan@gmail.com>
2022-06-29 19:27:15 +0430

    adding react app to project

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import {Routes, Router} from 'react-router-dom'
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <Routes>
+      <Router path='/' element={}/>
+    </Routes>
+    </div>
+  );
+}
+
+export default App;
