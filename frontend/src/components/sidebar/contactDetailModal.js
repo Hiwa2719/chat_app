54bf12daa935f2c915d8aa69b1e054a7452fa18e
hiwa2719 <hiahmadyan@gmail.com>
2022-07-14 17:26:35 +0430

    adding contact modal

---
+++ frontend/src/components/sidebar/contactDetailModal.js
@@ -0,0 +1,28 @@
+import React from "react";
+import '../../App.css'
+
+
+const ContactDetailModal = ({contact, setSelectedContact}) => {
+    const closeHandler = () => {
+        setSelectedContact('')
+    }
+
+    return (
+        <div className="modal-prop" onClick={() => setSelectedContact('')}>
+            <div className="modal-content w-25 p-3" onClick={(e)=> e.stopPropagation()}>
+                <div className="modal-header">
+                    <h5 className="modal-title">User Detail</h5>
+                    <button type="button" className="btn-close" data-bs-dismiss="modal"
+                            aria-label="Close" onClick={() => closeHandler()}></button>
+                </div>
+                <div className="mt-3">
+                    <h5>Username: {contact.username}</h5>
+                    <div className="mt-3">First name: {contact.first_name}</div>
+                    <div>Last name: {contact.last_name}</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ContactDetailModal
--- frontend/src/components/sidebar/contacts.js
+++ frontend/src/components/sidebar/contacts.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import {useSelector} from "react-redux";
 import Loader from "../loader";
 import Alert from "../alert";
+import ContactDetailModal from "./contactDetailModal";
 
 
 const Contacts = () => {
@@ -23,12 +24,15 @@ const Contacts = () => {
                 contacts &&
                 contacts.map(contact => (
                     <div key={contact.id}
-                         className={`ps-3 py-3 ${selectedContact === contact.id ? "item-clicked" : "sidebar-item"}`}
-                         onClick={() => setSelectedContact(contact.id)}>
+                         className={`ps-3 py-3 ${selectedContact.id === contact.id ? "item-clicked" : "sidebar-item"}`}
+                         onClick={() => setSelectedContact(contact)}>
                         <h5>@{contact.username}</h5>
                         <div>{contact.first_name} {contact.last_name}</div>
                     </div>
                 ))
+            }
+            {
+                selectedContact && <ContactDetailModal setSelectedContact={setSelectedContact} contact={selectedContact}/>
             }
         </div>
     )
