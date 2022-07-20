fc309d4dae0bf8581aa6188b249192cab63b5999
hiwa2719 <hiahmadyan@gmail.com>
2022-07-20 11:01:36 +0430

    adding redis to get contacts and remove contact

--- baseapp/views.py
+++ baseapp/views.py
@@ -1,3 +1,4 @@
+import redis, json
 from django.contrib.auth import get_user_model
 from django.shortcuts import render, redirect, reverse
 from rest_framework import status
@@ -10,6 +11,7 @@ from .serializers import MyTokenObtainPairSerializer, UserCreationSerializer, Us
     UserSerializerWithoutToken
 
 User = get_user_model()
+redis_client = redis.Redis(host='localhost', port=6379, db=0)
 
 
 def index_view(request):
@@ -57,27 +59,54 @@ def update_profile(request):
 @api_view()
 @permission_classes([IsAuthenticated])
 def user_chats(request):
-    chats = request.user.chat_set.all()
-    serializer = ChatSerializer(chats, many=True)
-    return Response(serializer.data)
+    key = f'{request.user.username}_chats'
+    if redis_client.exists(key):
+        string_chats = redis_client.get(key)
+        chats_queryset = json.loads(string_chats)
+    else:
+        chats_queryset = request.user.chat_set.all()
+        chats = ChatSerializer(chats_queryset, many=True).data
+        if chats:
+            redis_client.set(key, json.dumps(chats))
+    return Response(chats)
 
 
 @api_view()
 @permission_classes([IsAuthenticated])
 def get_contacts(request):
-    person = request.user.person
-    contacts = person.contacts.all()
-    serializer = UserSerializerWithoutToken(contacts, many=True)
-    return Response(serializer.data)
+    key = f'{request.user.username}_contacts'
+
+    if redis_client.exists(key):
+        print('hitting redis')
+        string_contacts = redis_client.get(key)
+        contacts = json.loads(string_contacts)
+    else:
+        print('hitting DB')
+        person = request.user.person
+        contacts_queryset = person.contacts.all()
+        serializer = UserSerializerWithoutToken(contacts_queryset, many=True)
+        contacts = serializer.data
+        redis_client.set(key, json.dumps(contacts))
+    return Response(contacts)
 
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def remove_contact(request):
     try:
-        contact = User.objects.get(id=request.data.get('id'))
+        contact_id = request.data.get('id')
+        contact = User.objects.get(id=contact_id)
     except User.DoesNotExist:
         return Response('this contact does not exists', status=status.HTTP_400_BAD_REQUEST)
     person = request.user.person
     person.contacts.remove(contact)
+    key = f'{request.user.username}_contacts'
+
+    if redis_client.exists(key):
+        print('removing from redis')
+        string_contacts = redis_client.get(key)
+        contacts = json.loads(string_contacts)
+        new_contacts = [contact for contact in contacts if contact['id'] != contact_id]
+        redis_client.set(key, json.dumps(new_contacts))
+        return Response(new_contacts)
     return redirect(reverse('baseapp:get-contacts'))
